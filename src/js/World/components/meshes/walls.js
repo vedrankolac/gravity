c6df5df6ac00e425a5aac00862b1c8dffff52ab3
birdlaketree <birdlaketree@gmail.com>
2023-03-01 14:56:54 +0100

    remove canvas maps from walls

--- src/js/World/components/meshes/walls.js
+++ src/js/World/components/meshes/walls.js
@@ -1,44 +1,25 @@
-import { PlaneGeometry, SphereGeometry, Mesh, MeshStandardMaterial, MathUtils, DoubleSide } from 'three';
-import { RndDotsFloor } from '../canvasMaps/RndDotsFloor';
-import { canvasTextureMaterial } from '../materials/canvasTextureMaterial';
+import { SphereGeometry, Mesh, MeshStandardMaterial, DoubleSide } from 'three';
 
-const walls = (scene, size, bgHSL, color) => {
-  const maps = new RndDotsFloor(bgHSL, color, 256);
-
+const walls = (scene, size, color) => {
   const plastic = {
     roughness: 1,
     metalness: 0,
     n: 'plastic'
   }
 
-  const materialFloor = canvasTextureMaterial(
-    {...maps},
-    plastic,
-    1
-  )
-
   const materialDome = new MeshStandardMaterial({
-    map: maps.colorMap,
-    normalMap: maps.normalMap,
-    envMapIntensity: 100,
+    color,
+    // map: maps.colorMap,
+    // normalMap: maps.normalMap,
+    // envMapIntensity: 100,
     side: DoubleSide,
     roughness: plastic.roughness,
     metalness: plastic.metalness,
   });
 
-  // const geometryPlane = new PlaneGeometry(size, size, 4, 4);
-  // const floor = new Mesh(geometryPlane, materialFloor);
-  // floor.receiveShadow = true;
-  // floor.rotation.x = MathUtils.degToRad(270);
-  // floor.position.y = -6;
-  // scene.add(floor);
-
   const geometryDome = new SphereGeometry(size/2, 64, 64);
   const dome = new Mesh(geometryDome, materialDome);
   scene.add(dome);
-
-  let mapsKeys = Object.keys(maps);
-  mapsKeys.forEach(k => maps[k] = null);
 }
 
 export { walls };
